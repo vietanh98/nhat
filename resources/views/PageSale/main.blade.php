cf28c478156fd7f13e552d22a41361650c00e317
Duc Phan <duc.phan@jmango360.com>
2025-03-22 10:39:15 +0700

    update

--- resources/views/PageSale/main.blade.php
+++ resources/views/PageSale/main.blade.php
@@ -4,14 +4,14 @@
 <head>
     <title>Mỹ phẩm Nhật Bản</title>
 </head>
-<link rel="stylesheet" type="text/css" href="{{asset('pageSale/boostrap/css/bootstrap.min.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('pageSale/boostrap/css/bootstrap.css')}}">
-<link rel="stylesheet" href="{{asset('pageSale/css/style.css')}}">
-<link rel="stylesheet" href="{{asset('pageSale/css/effect.css')}}">
+<link rel="stylesheet" type="text/css" href="{{secure_asset('pageSale/boostrap/css/bootstrap.min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{secure_asset('pageSale/boostrap/css/bootstrap.css')}}">
+<link rel="stylesheet" href="{{secure_asset('pageSale/css/style.css')}}">
+<link rel="stylesheet" href="{{secure_asset('pageSale/css/effect.css')}}">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="{{asset('pageSale/slider/dist/assets/owl.carousel.min.css')}}">
-<link rel="stylesheet" href="{{asset('pageSale/slider/dist/assets/owl.theme.default.min.css')}}">
+<link rel="stylesheet" href="{{secure_asset('pageSale/slider/dist/secure_assets/owl.carousel.min.css')}}">
+<link rel="stylesheet" href="{{secure_asset('pageSale/slider/dist/secure_assets/owl.theme.default.min.css')}}">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
       integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
